6af3576f301d00f27c94b25d81b0b0b5d5b3a922
sopan11 <140255563+sopan11@users.noreply.github.com>
2024-06-08 21:08:05 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,19 @@
+function addition(num1, num2){
+    let result = num1+num2;
+    return result;
+}
+
+result1 = addition(3, 6);
+result2 = addition(1, 8);
+result3 = addition(9, 7)
+
+console.log("Addition is: ",result1); //9
+console.log("Addition is: ",result2); //9
+console.log("Addition is: ",result3); //16
+
+//Arrow function
+const function1 =(x) =>{
+    console.log("Value of x is: ",x)
+}
+
+console.log(45);
